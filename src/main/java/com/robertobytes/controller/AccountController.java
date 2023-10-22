950615e21584ff620b42f527b8f47417c90500f8
robertocannella <robertocannella@gmail.com>
2023-10-22 14:32:34 -0400

    Initial commit

---
+++ src/main/java/com/robertobytes/controller/AccountController.java
@@ -0,0 +1,13 @@
+package com.robertobytes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/myAccount")
+    public String myAccount(){
+        return  "My Account Details";
+    }
+}
---
+++ src/test/java/com/robertobytes/SpringSecurityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robertobytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
